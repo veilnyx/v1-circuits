bd73a81eb2cabc8da615bf470a2f1ba4ba6777bc
theNvN <thenvnsahu@gmail.com>
2023-07-20 21:49:48 +0530

    feat: add `enabled` input for merkle proof

--- circuits/lib/merkleProof.circom
+++ circuits/lib/merkleProof.circom
@@ -5,6 +5,7 @@ include "../../node_modules/circomlib/circuits/poseidon.circom";
 include "../../node_modules/circomlib/circuits/switcher.circom";
 
 template MerkleProof(nLevels) {
+    signal input enabled;
     signal input root;
     signal input leaf;
     signal input pathElements[nLevels];
@@ -27,5 +28,8 @@ template MerkleProof(nLevels) {
         hasher[i].inputs[1] <== switcher[i].outR;
     }
 
-    root === hasher[nLevels - 1].out;
+    component checkRoot = ForceEqualIfEnabled();
+    checkRoot.enabled <== enabled;
+    checkRoot.in[0] <== root;
+    checkRoot.in[1] <== hasher[nLevels - 1].out;
 }
--- tests/merkleProof.spec.ts
+++ tests/merkleProof.spec.ts
@@ -14,7 +14,7 @@ describe('merkleProof', function () {
     circuit = await getCircuit('merkleProof20');
   });
 
-  it('should verify correct inclusion path', async function () {
+  it('should verify correct inclusion path when enabled', async function () {
     const tree = getTree();
     const leaves = [randomLeaf(), randomLeaf(), randomLeaf(), randomLeaf()];
     tree.bulkInsert(leaves);
@@ -25,6 +25,7 @@ describe('merkleProof', function () {
     const pathElements = tree.path(idx).pathElements;
 
     const inputs = {
+      enabled: 1,
       leaf: proofElement,
       pathElements,
       pathIndices: idx,
@@ -37,7 +38,32 @@ describe('merkleProof', function () {
     await circuit.checkConstraints(witness);
   });
 
-  it('should fail verification for incorrect inclusion path', async function () {
+  it('should verify with incorrect when disabled', async function () {
+    const tree = getTree();
+    const leaves = [randomLeaf(), randomLeaf(), randomLeaf(), randomLeaf()];
+    tree.bulkInsert(leaves);
+
+    const root = BigNumber.from(tree.root).toHexString();
+    const badRoot = randomHex(32);
+    const proofElement = leaves[1];
+    const idx = tree.indexOf(proofElement);
+    const pathElements = tree.path(idx).pathElements;
+
+    const inputs = {
+      enabled: 0,
+      leaf: proofElement,
+      pathElements,
+      pathIndices: idx,
+      root: badRoot,
+    };
+
+    await assert.isFulfilled(circuit.calculateWitness(inputs, true));
+
+    const witness = await circuit.calculateWitness(inputs);
+    await circuit.checkConstraints(witness);
+  });
+
+  it('should fail verification for incorrect inclusion path when enabled', async function () {
     const tree = getTree();
     const leaves = [randomLeaf(), randomLeaf(), randomLeaf()];
     tree.bulkInsert(leaves);
@@ -49,10 +75,34 @@ describe('merkleProof', function () {
     const pathElements = tree.path(idx).pathElements;
 
     const inputs = {
+      enabled: 1,
       leaf: badProofElement,
       pathElements,
       pathIndices: idx,
       root,
+    };
+
+    await assert.isRejected(circuit.calculateWitness(inputs, true), Error);
+  });
+
+  it('should fail verification for incorrect root when enabled', async function () {
+    const tree = getTree();
+    const leaves = [randomLeaf(), randomLeaf(), randomLeaf()];
+    tree.bulkInsert(leaves);
+
+    const root = BigNumber.from(tree.root).toHexString();
+    const badRoot = randomHex(32);
+    const proofElement = leaves[1];
+    const badProofElement = 3;
+    const idx = tree.indexOf(proofElement);
+    const pathElements = tree.path(idx).pathElements;
+
+    const inputs = {
+      enabled: 1,
+      leaf: badProofElement,
+      pathElements,
+      pathIndices: idx,
+      root: badRoot,
     };
 
     await assert.isRejected(circuit.calculateWitness(inputs, true), Error);
