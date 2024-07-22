b6f1a6868005a5eaa5e606d05b34bca46f568d87
nvnx7 <thenvnsahu@gmail.com>
2024-07-22 12:50:00 +0530

    feat: dynamically choose appropriate pTau file for circuit

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,7 +1,29 @@
 const config = {
   srcDir: 'circuits',
   outDir: 'artifacts',
-  pTauUrl: `https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_18.ptau`,
+  pTauUrls: {
+    8: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_08.ptau',
+    9: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_09.ptau',
+    10: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_10.ptau',
+    11: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_11.ptau',
+    12: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_12.ptau',
+    13: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_13.ptau',
+    14: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_14.ptau',
+    15: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_15.ptau',
+    16: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_16.ptau',
+    17: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_17.ptau',
+    18: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_18.ptau',
+    19: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_19.ptau',
+    20: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_20.ptau',
+    21: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_21.ptau',
+    22: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_22.ptau',
+    23: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_23.ptau',
+    24: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_24.ptau',
+    25: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_25.ptau',
+    26: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_26.ptau',
+    27: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_27.ptau',
+    28: 'https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final.ptau',
+  },
 };
 
 export default config;
