2c524dd1af6c2c7ffec126da75c977d0345abe72
nvnx7 <thenvnsahu@gmail.com>
2024-08-12 03:17:57 +0530

    feat: subtree update by pair leaf insertions

--- tests/subtreeUpdate.spec.ts
+++ tests/subtreeUpdate.spec.ts
@@ -2,64 +2,87 @@ import { assert, expect } from 'chai';
 import { hexToBigInt, keccak256, stringToBytes } from 'viem';
 import { MerkleTree } from 'fixed-merkle-tree';
 import { randomBigInt } from '@zkfi-tech/utils';
+import { poseidonHash } from '@zkfi-tech/babyjubjub';
 import { fieldsSize, getCircuit, getMerkleTree, toPaddedHex } from './helpers';
 
 const treeDepth = 32;
-const zeroLeaf = hexToBigInt(keccak256(stringToBytes('zkFi'))) % BigInt(fieldsSize);
+const zeroLeaf = hexToBigInt(keccak256(stringToBytes('zero'))) % BigInt(fieldsSize);
 const getTree = () => getMerkleTree(treeDepth, [], zeroLeaf);
+
+const getInitialTreeState = () => {
+  let z = zeroLeaf;
+  const lastSubtree: bigint[] = [];
+  const zeros: bigint[] = [];
+  for (let i = 0; i < treeDepth; i++) {
+    zeros.push(z);
+    lastSubtree.push(z);
+    z = poseidonHash([z, z]);
+  }
+  const root = z;
+
+  return { lastSubtree, zeros, root };
+};
+
 const getLastSubtree = (tree: MerkleTree) => {
+  const zeros = getInitialTreeState().zeros;
   const nLevels = tree.levels;
   const subtree: any[] = [];
 
-  let lastLeafIndex = tree.elements.length - 1;
-  for (let i = 0; i < nLevels; i++) {
-    if (lastLeafIndex % 2 === 0) {
-      subtree[i] = tree.layers[i]?.[lastLeafIndex] || zeroLeaf;
+  const nextLeafIndex = tree.elements.length;
+  let currentLevelIndex = nextLeafIndex - 1;
+  currentLevelIndex = Math.floor(currentLevelIndex / 2);
+
+  subtree[0] = zeros[0];
+  for (let i = 1; i < nLevels; i++) {
+    if (currentLevelIndex % 2 === 0) {
+      // Left/Even
+      subtree[i] = tree.layers[i]?.[currentLevelIndex] || zeros[i];
     } else {
-      subtree[i] = tree.layers[i]?.[lastLeafIndex - 1] || zeroLeaf;
+      // Right/Odd
+      subtree[i] = tree.layers[i]?.[currentLevelIndex - 1] || zeros[i];
     }
 
-    lastLeafIndex = Math.floor(lastLeafIndex / 2);
+    currentLevelIndex = Math.floor(currentLevelIndex / 2);
   }
 
   return subtree.map((el) => BigInt(el));
 };
 
-const randomLeaf = () => toPaddedHex(randomBigInt(32));
+const randomLeaf = () => toPaddedHex(randomBigInt(31));
 
 describe('subtreeUpdate', function () {
   this.timeout(20000);
-  let insertLeafCircuit;
+  let insertLeavesPairCircuit;
   let updateTreeCircuit;
 
   before(async function () {
-    insertLeafCircuit = await getCircuit('insertLeaf');
+    insertLeavesPairCircuit = await getCircuit('insertLeavesPair');
     updateTreeCircuit = await getCircuit('subtreeUpdate');
   });
 
-  it('should correctly insert leaf', async function () {
+  it('should correctly insert leaf pairs', async function () {
     const tree = getTree();
-    const leaves = Array.from({ length: 10 }).map(() => randomLeaf());
-    tree.bulkInsert(leaves);
+    const initialLeaves = Array.from({ length: 10 }).map(() => randomLeaf());
+    tree.bulkInsert(initialLeaves);
 
-    const leafIndex = tree.elements.length;
     const lastSubtree = getLastSubtree(tree);
     const lastRoot = BigInt(tree.root.toString());
 
-    const leaf = randomLeaf();
+    const leafIndex = tree.elements.length;
+    const leaves = [randomLeaf(), randomLeaf()];
+    tree.bulkInsert(leaves);
 
-    tree.insert(leaf);
     const newSubtree = getLastSubtree(tree);
     const newRoot = BigInt(tree.root.toString());
 
     const inputs = {
-      leaf,
+      leaves,
       leafIndex,
       lastRoot,
       lastSubtree,
     };
 
-    const witness = await insertLeafCircuit.calculateWitness(inputs);
+    const witness = await insertLeavesPairCircuit.calculateWitness(inputs);
 
     expect(witness[1]).to.equal(newRoot);
     for (let i = 0; i < newSubtree.length; i++) {
